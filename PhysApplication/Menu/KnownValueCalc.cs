66bedfce019fe397756192d960b70fe49455503a
Damien Stamates <thedoubledimensions@gmail.com>
2017-10-09 21:35:04 -0500

    Refactoring

--- PhysApplication/Menu/KnownValueCalc.cs
+++ PhysApplication/Menu/KnownValueCalc.cs
@@ -20,23 +20,24 @@ namespace PhysApplication.Menu
         public static int ScreenNum = 1;
         public void CreateMenu()
         {
-            Console.Clear();
+            Console.Clear(); // Refreshes the console
             foreach(variableMenuObject a in categ) {
-                Console.WriteLine((a.Checked?"(x)":a.Selected?"(-)":"( )")+a.Name+(a.Selected?" <-":""));
+                Console.WriteLine((a.Checked?"(x)":a.Selected?"(-)":"( )")+a.Name+(a.Selected?" <-":"")); // Prints out each individual item on the list.
             }
+            // Setting up the footer of each page.
             Console.Write(ScreenNum==1?"":ScreenNum==2?"\n\n\n\n\n\n\n\n":ScreenNum==3?"\n\n\n\n\n\n\n\n\n\n":ScreenNum==4?"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n":ScreenNum==5?"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n":ScreenNum==6?"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n":"");
             Console.Write("------(Spacebar to Select)------(Enter to Proceed)------(Pages {1,2,3,4,5,6})------(Current Page: " + (ScreenNum == 1 ? "Mechanics" : ScreenNum == 2 ? "Electric/Magnetism" : ScreenNum == 3 ? "Fluid/Thermal" : ScreenNum == 4 ? "Geometry/Trig" : ScreenNum == 5 ? "Modern" : "Waves/Optics") + ")------");
         }
         private void KeyAnalyze(string key) {
-            if (key.ToUpper() == "DOWNARROW") Move(false);
-            if (key.ToUpper() == "UPARROW") Move(true);
-            if (key.ToUpper() == "SPACEBAR") Select();
-            if (key == "D1") { if (ScreenNum != 1) categ = MechList.MechanicsVariables; ScreenNum = 1; }
-            if (key == "D2") { if (ScreenNum != 2) categ = ElecList.ElectricAndMagnetismVariables; ScreenNum = 2; }
-            if (key == "D3") { if (ScreenNum != 3) categ = FluiList.FluidMechanicsAndThermalPhysicsVariables; ScreenNum = 3; }
-            if (key == "D4") { if (ScreenNum != 4) categ = GeoList.GeometryAndTrigonometryVariables; ScreenNum = 4; }
-            if (key == "D5") { if (ScreenNum != 5) categ = ModernList.ModernPhysicsVariables; ScreenNum = 5; }
-            if (key == "D6") { if (ScreenNum != 6) categ = WaveList.WavesAndOpticsVariables; ScreenNum = 6; }
+            if (key.ToUpper() == "DOWNARROW") Move(false); // Calls for Move and sets the bool (upMove) to false
+            if (key.ToUpper() == "UPARROW") Move(true);    // Calls for Move and sets the bool (upMove) to true 
+            if (key.ToUpper() == "SPACEBAR") Select();     // Selects object
+            if (key == "D1") { if (ScreenNum != 1) categ = MechList.MechanicsVariables; ScreenNum = 1; }                       // Checks if same screen. If not then it sets the list to list #1
+            if (key == "D2") { if (ScreenNum != 2) categ = ElecList.ElectricAndMagnetismVariables; ScreenNum = 2; }            // Ditto
+            if (key == "D3") { if (ScreenNum != 3) categ = FluiList.FluidMechanicsAndThermalPhysicsVariables; ScreenNum = 3; } // Ditto
+            if (key == "D4") { if (ScreenNum != 4) categ = GeoList.GeometryAndTrigonometryVariables; ScreenNum = 4; }          // Ditto
+            if (key == "D5") { if (ScreenNum != 5) categ = ModernList.ModernPhysicsVariables; ScreenNum = 5; }                 // Ditto
+            if (key == "D6") { if (ScreenNum != 6) categ = WaveList.WavesAndOpticsVariables; ScreenNum = 6; }                  // Ditto
         }
         private void Select() {
             for(int i = 0; i < categ.Count(); i++) {
